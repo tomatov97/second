94be867c5614260885573f513ab292a8566433be
DESKTOP-67MFFSP\USER <tngusdl30@gmail.com>
2022-04-08 13:29:36 +0900

    Ex01.java -> for반복문으로 출력되도록 수정

--- web_3/src/main/java/etc/Ex01.java
+++ web_3/src/main/java/etc/Ex01.java
@@ -4,17 +4,11 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
-		System.out.println("Hello World~!");
+		
+		for (int i=0; i<10; i++) {
+			System.out.println("Hello World~!");
+		}
+		
 		
 	}
 
