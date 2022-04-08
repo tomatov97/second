12f8435615fb917adf440fb2a1bb009b2c22f9a1
DESKTOP-67MFFSP\USER <tngusdl30@gmail.com>
2022-04-08 14:47:20 +0900

    Ex02.java -> 충동을 만들기 위해 소스코드 수정

--- web_3/src/main/java/etc/Ex02.java
+++ web_3/src/main/java/etc/Ex02.java
@@ -5,7 +5,7 @@ public class Ex02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world~!");
+		System.out.println("안녕하세요!");
 	}
 
 }
